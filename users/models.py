5635059d934c609e9c43520a9743e7636147a933
dmitrijkalakin <weeapa@mail.ru>
2022-10-03 11:32:09 +0300

    fix admin auth, add filed email in admin

--- users/models.py
+++ users/models.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     class Meta():
         verbose_name = "пользователь"
