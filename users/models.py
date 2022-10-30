81a6705d17f082dee819a7837245f1063cc3489c
dmitrijkalakin <weeapa@mail.ru>
2022-10-30 18:19:56 +0300

    start new functions

--- users/models.py
+++ users/models.py
@@ -3,14 +3,20 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Role(models.TextChoices):
+    """Роли пользователей"""
+
+    CUSTOMER = 'CUSTOMER', 'клиент'
+    WORKER = 'WORKER', 'слесарь'
+    MANAGER = 'MANAGER', 'управляющий'
+    MASTER = 'MASTER', 'мастер'
+
+
 class User(AbstractUser):
     email = models.EmailField(
         _("email address"),
         unique=True,
     )
-
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
-    email_verify = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
